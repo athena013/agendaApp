6bbefb32ed8d163ea9ed317d0b4238c73e74642a
Laura Estefania Gómez Muñoz <athena013@gmail.com>
2018-05-20 11:40:39 -0500

    editado paramsApp.js

--- agendaFront/lib/paramsApp.js
+++ agendaFront/lib/paramsApp.js
@@ -1,10 +1,10 @@
 var ambiente = 'local';
 var urlParametrosModule = getUrlServidor();
-var urlFront = 'http://localhost:88/Apoyo_escolar/agenda/agendaFront/';
+var urlFront = 'http://localhost:88/agenda/agendaFront/';
 var urlBackEnd = getUrlServidor();
 function getUrlServidor() {
     if (ambiente === 'local') {
-        return 'http://localhost:88/Apoyo_escolar/agenda/agendaBE/index.php/';
+        return 'http://localhost:88/agenda/agendaBE/';
     }else if (ambiente === 'desarrollo') {
         return 'http://192.168.1.41:80/agenda/agendaBE/';
     }
