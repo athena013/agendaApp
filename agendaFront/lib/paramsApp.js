caa4cf8fee80a31edb7745caa50b29b652f045ab
Laura Estefania Gómez Muñoz <athena013@gmail.com>
2018-05-30 17:00:58 -0500

    version en mi repositorio

--- agendaFront/lib/paramsApp.js
+++ agendaFront/lib/paramsApp.js
@@ -1,10 +1,13 @@
 var ambiente = 'local';
 var urlParametrosModule = getUrlServidor();
-var urlFront = 'http://localhost:80/agenda/agendaFront/';
+
+var urlFront = 'http://localhost:88/agenda/agendaFront/';
+
 var urlBackEnd = getUrlServidor();
+
 function getUrlServidor() {
     if (ambiente === 'local') {
-        return 'http://localhost:80/agenda/agendaBE/';
+        return 'http://localhost:88/agenda/agendaBE/';
     }else if (ambiente === 'desarrollo') {
         return 'http://192.168.1.41:80/agenda/agendaBE/';
     }
